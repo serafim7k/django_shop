fcd9b837daa238c635e49b24ebb1fd4e596cf445
serafim7k <mycomputer10j12@gmail.com>
2025-03-23 19:49:02 +0200

    Add Media

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+from app import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
@@ -25,8 +28,5 @@ urlpatterns = [
     
 ] + debug_toolbar_urls()
 
-# import debug_toolbar
-
-# urlpatterns += [ path('__debug__/', include(debug_toolbar.urls)),]
-
-# urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
